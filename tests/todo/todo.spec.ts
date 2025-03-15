ea751bba38c0fd4ddac0fb17dcb954a6026c571c
RattaponOudtagosum <rattapon.ot@gmail.com>
2025-03-15 14:54:58 +0700

    Update todo.spec.ts

--- tests/todo/todo.spec.ts
+++ tests/todo/todo.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('To do list', async ({ page }) => {
     await page.goto('https://rtp-ots.github.io/todolist/examples/react/dist/');
-    await page.getByTestId('text-input').fill('write a blogs')
+    await page.getByTestId('text-input').fill('demo')
     await page.getByTestId('text-input').press('Enter')
     await page.getByTestId('todo-item-toggle').click()
     await expect(page.getByTestId('todo-item-label')).toBeVisible();
